2f21013f1c4ecac2f16f7def5772d315423ecd53
Jonathan Henrique de Moura <jonathanmourax4@gmail.com>
2022-01-27 07:24:35 -0300

    commit

---
+++ 19 - Testes Automatizados/1 - Introdução/introducao_test.go
@@ -0,0 +1,32 @@
+package introducao
+
+import (
+	"testing"
+)
+
+type cenarioDeTeste struct {
+	enderecoInserido string
+	retornoEsperado  string
+}
+
+func TestTipoDeEndereco(t *testing.T) {
+
+	cenariosDeTeste := []cenarioDeTeste{
+		{"Rua ABC", "Rua"},
+		{"Avenida Paulista", "Avenida"},
+		{"Rodovia dos Imigrantes", "Rodovia"},
+		//	{"Praça da roda", "Tipo Inválido"},
+		{"Estrada aleatoria", "Estrada"},
+		//	{"", "Tipo Inválido"},
+		{"RUA TESTE", "Rua"},
+	}
+
+	for _, cenario := range cenariosDeTeste {
+		tipoDeEnderecoRecebido := TipoDeEndereco(cenario.enderecoInserido)
+
+		if tipoDeEnderecoRecebido != cenario.retornoEsperado {
+			t.Errorf("O tipo recebido %s é diferente do esperado %s", tipoDeEnderecoRecebido, cenario.retornoEsperado)
+		}
+	}
+
+}
---
+++ 22 - HTML/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+var templates *template.Template
+
+type usuario struct {
+	Nome  string
+	Email string
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+
+	u := usuario{
+		"Joao",
+		"Joao@gmail.com",
+	}
+
+	templates.ExecuteTemplate(w, "home.html", u)
+}
+
+func main() {
+
+	templates = template.Must(template.ParseGlob("*.html"))
+
+	http.HandleFunc("/home", home)
+
+	fmt.Println("Escutando na porta 5000")
+	log.Fatal(http.ListenAndServe(":5000", nil))
+}
